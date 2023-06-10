afe8fa22beb8f8b7f633c99ad85e779fcce3d213
Jose Wills <jose.wills3@gmail.com>
2023-06-10 08:54:57 +0100

    added and passed location details test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
+import forecast from "../data/forecast.json";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<App location={forecast.location} />);
   const linkElement = screen.getByText(/weather app/i);
   expect(linkElement).toBeInTheDocument();
 });
